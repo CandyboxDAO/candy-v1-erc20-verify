14391eb64ba4603e8130cff50cc360d2a51b8a29
Exekias <odd_amphora@protonmail.com>
2021-11-27 21:02:30 -0500

    Stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,13 @@
-const fs = require('fs');
 const dotenv = require('dotenv');
-const taskNames = require('hardhat/builtin-tasks/task-names');
 
 require('@nomiclabs/hardhat-etherscan');
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('hardhat-gas-reporter');
-require('hardhat-deploy');
 
 dotenv.config();
 
 const defaultNetwork = 'localhost';
-
-function mnemonic() {
-  try {
-    return fs.readFileSync('./mnemonic.txt').toString().trim();
-  } catch (e) {
-    if (defaultNetwork !== 'localhost') {
-      console.log('☢️ WARNING: No mnemonic file created for a deploy account.');
-    }
-  }
-  return '';
-}
-
 const infuraId = process.env.INFURA_ID;
 
 module.exports = {
@@ -54,6 +39,7 @@ module.exports = {
       default: 0,
     },
   },
+  // WARNING: Do not change //
   solidity: {
     version: '0.8.6',
     settings: {
@@ -62,6 +48,7 @@ module.exports = {
         runs: 10000,
       },
     },
+  // End Warning /////////////
   },
   mocha: {
     bail: true,
@@ -77,61 +64,3 @@ module.exports = {
     apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
 };
-
-// List details of deployer account.
-task('account', 'Get balance informations for the deployment account.', async (_, { ethers }) => {
-  const hdkey = require('ethereumjs-wallet/hdkey');
-  const bip39 = require('bip39');
-  let mnemonic = fs.readFileSync('./mnemonic.txt').toString().trim();
-  const seed = await bip39.mnemonicToSeed(mnemonic);
-  const hdwallet = hdkey.fromMasterSeed(seed);
-  const wallet_hdpath = "m/44'/60'/0'/0/";
-  const account_index = 0;
-  let fullPath = wallet_hdpath + account_index;
-  const wallet = hdwallet.derivePath(fullPath).getWallet();
-  var EthUtil = require('ethereumjs-util');
-  const address = '0x' + EthUtil.privateToAddress(wallet._privKey).toString('hex');
-
-  console.log('Deployer Account: ' + address);
-  for (let n in config.networks) {
-    try {
-      let provider = new ethers.providers.JsonRpcProvider(config.networks[n].url);
-      let balance = await provider.getBalance(address);
-      console.log(' -- ' + n + ' --  -- -- 📡 ');
-      console.log('   balance: ' + ethers.utils.formatEther(balance));
-      console.log('   nonce: ' + (await provider.getTransactionCount(address)));
-    } catch (e) {
-      console.log(e);
-    }
-  }
-});
-
-task('compile:one', 'Compiles a single contract in isolation')
-  .addPositionalParam('contractName')
-  .setAction(async function (args, env) {
-    const sourceName = env.artifacts.readArtifactSync(args.contractName).sourceName;
-
-    const dependencyGraph = await env.run(taskNames.TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH, {
-      sourceNames: [sourceName],
-    });
-
-    const resolvedFiles = dependencyGraph.getResolvedFiles().filter((resolvedFile) => {
-      return resolvedFile.sourceName === sourceName;
-    });
-
-    const compilationJob = await env.run(
-      taskNames.TASK_COMPILE_SOLIDITY_GET_COMPILATION_JOB_FOR_FILE,
-      {
-        dependencyGraph,
-        file: resolvedFiles[0],
-      },
-    );
-
-    await env.run(taskNames.TASK_COMPILE_SOLIDITY_COMPILE_JOB, {
-      compilationJob,
-      compilationJobs: [compilationJob],
-      compilationJobIndex: 0,
-      emitsArtifacts: true,
-      quiet: true,
-    });
-  });
